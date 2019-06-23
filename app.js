bb225f4300cb8b22f297b136515be19d191aa53e
chenzhushuaiqi <1120480450@qq.com>
2019-06-23 22:17:54 +0800

    登录

--- app.js
+++ app.js
@@ -57,14 +57,11 @@ app.post('/register', (req, res) => {
     if (body.username.trim().length <= 0 || body.password.trim().length <= 0 || body.nickname.trim().length <= 0) {
         return res.send({ msg: '请填写完整的表单信息后再注册', status: 501 })
     }
-    console.log(body.username);
 
     // 查询用户名是否重复
     const sql1 = 'select count(*) as count from users where username=?'
     conn.query(sql1, body.username, (err, result) => {
         // 如果查询失败，则告知客户端失败。 :
-        // console.log(result);
-        console.log(err);
 
         if (err) return res.send({ msg: '用户名查重失败', status: 502 })
         if (result[0].count !== 0) return res.send({ msg: '用户名存在', status: 503 })
@@ -73,14 +70,37 @@ app.post('/register', (req, res) => {
         // 执行注册的业务逻辑
         body.ctime = moment().format('YYYY-MM-DD HH:mm:ss')
         const sql2 = 'insert into users set ?'
-        console.log(body);
-
         conn.query(sql2, body, (err, result) => {
-            if (err) return res.send({ status: 505, msg: err.message, data: null })
-            res.send({ status: 200, msg: 'ok' })
+            if (err) return res.send({ msg: '注册新用户失败', status: 504 });
+            if (result.affectedRows !== 1) return res.send({ msg: '注册新用户失败', status: 505 })
+            res.send({ status: 200, msg: '注册成功' })
+
+
         })
 
+
     })
+})
+
+// 用户登录
+app.post('/login', (req, res) => {
+
+    // 获取用书输入的数据
+    const body = req.body;
+    // console.log(body);
+
+    // 执行sql语句  查询用户在数据库中是否存在
+    const sql1 = 'select * from users where username=? and password=?'
+    conn.query(sql1, [body.username, body.password], (err, result) => {
+        if (err) return res.send({ msg: '用户登录失败', status: 601 });
+        // console.log(result);
+
+        if (result.length !== 1) return res.send({ msg: '用户登录失败', status: 602 })
+
+        res.send({ status: 200, msg: '登录成功' })
+
+    })
+
 })
 
 app.listen(3000, () => {
